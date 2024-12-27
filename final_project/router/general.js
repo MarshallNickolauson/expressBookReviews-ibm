d62aa03fb73d083cf5e01d745ccc970190bfea3c
marshall nickolauson <marshnickol101@gmail.com>
2024-12-27 10:37:20 -0500

    get book review by id

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,8 +45,10 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: 'Yet to be implemented' });
+    const bookDetails = books[req.params.isbn];
+    if (!bookDetails)
+        return res.status(404).json({ message: 'Book not found' });
+    return res.status(200).json(bookDetails.reviews);
 });
 
 module.exports.general = public_users;
