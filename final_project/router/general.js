bf761cebefaa54477a3665a0a2e18951ffd5ddac
marshall nickolauson <marshnickol101@gmail.com>
2024-12-27 10:31:10 -0500

    get book by author or title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,20 +16,31 @@ public_users.get('/', function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     const bookDetails = books[req.params.isbn];
-    if (!bookDetails) return res.status(404).json({message: 'Book not found'})
+    if (!bookDetails)
+        return res.status(404).json({ message: 'Book not found' });
     return res.status(200).json(bookDetails);
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: 'Yet to be implemented' });
+    for (let i = 1; i <= 10; i++) {
+        const book = books[i];
+        if (book.author === req.params.author) {
+            return res.status(200).json(book);
+        }
+    }
+    return res.status(404).json({ message: 'Author not found' });
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: 'Yet to be implemented' });
+    for (let i = 1; i <= 10; i++) {
+        const book = books[i];
+        if (book.title === req.params.title) {
+            return res.status(200).json(book);
+        }
+    }
+    return res.status(404).json({ message: 'Title not found' });
 });
 
 //  Get book review
