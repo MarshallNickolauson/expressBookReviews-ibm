0e4c7a2eddd96ec0b820b60981d7398df45b069a
marshall nickolauson <marshnickol101@gmail.com>
2024-12-28 20:37:37 -0500

    update book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -44,11 +44,36 @@ regd_users.post('/login', (req, res) => {
     }
 });
 
-// Add a book review
 regd_users.put('/auth/review/:isbn', (req, res) => {
-    //Write your code here
-    return res.status(300).json({ message: 'Yet to be implemented' });
+    const isbn = req.params.isbn;
+    const username = req.session.authorization?.username;
+    const review = req.body.review;
+
+    console.log(req.session)
+    console.log(req.params.isbn)
+
+    if (!username) {
+        return res.status(401).json({ message: 'User not logged in' });
+    }
+
+    if (!books[isbn]) {
+        return res.status(404).json({ message: 'Book not found' });
+    }
+
+    if (!review) {
+        return res.status(400).json({ message: 'Review content missing' });
+    }
+
+    const book = books[isbn];
+    if (!book.reviews) {
+        book.reviews = {};
+    }
+
+    book.reviews[username] = review;
+
+    return res.status(200).json({ message: 'Review successfully added/updated', reviews: book.reviews });
 });
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
